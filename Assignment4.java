aee764330ae3ef1bf44b58e962d7721203463407
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-02-13 11:19:11 -0700

    Update Assignment4.java
    I removed magic numbers with constants

--- Assignment4.java
+++ Assignment4.java
@@ -18,7 +18,12 @@ public class Assignment4 extends Application {
   private double newDeposit = 0;
   private double newWithdrawl = 0;
   private double accountBalance = bankAccount.getBalance();
-
+  private final int HBOXWIDTH = 10;
+  private final int HBOXWIDTH = 30;
+  private final int SCREENHEIGHT = 300;
+  private final int SCREENWIDTH = 500;
+  private final int TEXTFIELDWIDTH = 100;
+  
 
   //Added a setCustomer method - Dayan
   public void setUserCustomer() {
@@ -45,15 +50,15 @@ public class Assignment4 extends Application {
     StackPane root = new StackPane();
 
     //Vbox containing all hboxes
-    VBox vBox = new VBox(30);
+    VBox vBox = new VBox(VBOXWIDTH);
 
     //Hbox containing the balance and a label for it
-    HBox balanceBox = new HBox(10);
+    HBox balanceBox = new HBox(HBOXWIDTH);
     Label statBalance = new Label("Balance: " + accountBalance);
     balanceBox.getChildren().add(statBalance);
 
     //Hbox containing Customer info
-    HBox accountHolderBox = new HBox(10);
+    HBox accountHolderBox = new HBox(HBOXWIDTH);
     Label accountHolderName = new Label("Account Holder Name: " + customer.getName());
     Label accountHolderID = new Label("Account ID: " + customer.getID());
     accountHolderBox.getChildren().addAll(accountHolderName, accountHolderID);
@@ -67,7 +72,7 @@ public class Assignment4 extends Application {
     //Vbox Containing the user input area
     VBox changeInMoney = new VBox();
     TextField entry = new TextField("Enter Withdrawl or Deposit Amount");
-    entry.setPrefWidth(100);
+    entry.setPrefWidth(TEXTFIELDWIDTH);
     changeInMoney.getChildren().add(entry);
 
     //Create actions for buttons
@@ -85,7 +90,7 @@ public class Assignment4 extends Application {
 
     vBox.getChildren().addAll(accountHolderBox, balanceBox, buttons, changeInMoney);
     root.getChildren().add(vBox);
-    Scene scene = new Scene(root, 500, 300);
+    Scene scene = new Scene(root, SCREENWIDTH, SCREENHEIGHT);
 
     primaryStage.setTitle("BankAccount");
     primaryStage.setScene(scene);
